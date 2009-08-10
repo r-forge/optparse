a16a685da21f3b5cf57905c0ae96eec5a677cf9d
trevorld (Trevor Davis) <trevor.l.davis@gmail.com>
2009-08-10 20:53:00 +0000

    Added text to the vignette
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/optparse@29 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/optparse/inst/doc/Rscripts/example.R
+++ pkg/optparse/inst/doc/Rscripts/example.R
@@ -4,33 +4,38 @@
 suppressPackageStartupMessages(library("optparse"))
 
 # specify our desired options in a list
-# by default ``OptionParser`` will automatically add an help option equivalent to 
-# ``make_option(c("-h", "--help"), action="store_true", default=FALSE, 
-#               help="Show this help message and exit")``
+# by default OptionParser will add an help option equivalent to 
+# make_option(c("-h", "--help"), action="store_true", default=FALSE, 
+#               help="Show this help message and exit")
 option_list <- list( 
     make_option(c("-v", "--verbose"), action="store_true", default=TRUE,
         help="Print extra output [default]"),
     make_option(c("-q", "--quietly"), action="store_false", 
         dest="verbose", help="Print little output"),
-    make_option(c("-c", "--count"), action="store", type="integer", default=5, 
+    make_option(c("-c", "--count"), type="integer", default=5, 
         help="Number of random normals to generate [default %default]",
         metavar="number"),
-    make_option("--mean", action="store", type="numeric", default=0,
-        help="Mean of random normals [default %default]"),
-    make_option("--sd", action="store", type="numeric", default=1,
-        help="Standard deviation of random normals [default %default]",
-        metavar="standard_deviation")
+    make_option("--generator", default="rnorm", 
+        help = "Function to generate random deviates [default \"%default\"]"),
+    make_option("--mean", default=0, 
+        help="Mean if generator == \"rnorm\" [default %default]"),
+    make_option("--sd", default=1, metavar="standard deviation",
+        help="Standard deviation if generator == \"rnorm\" [default %default]")
     )
                                         
 # get command line options, if help option encountered print help and exit,
-# otherwise if options not found on command line then set to pre-specified defaults, 
+# otherwise if options not found on command line then set defaults, 
 opt <- parse_args(OptionParser(option_list=option_list))
 
 # print some progress messages to stderr if "quietly" wasn't requested
 if ( opt$verbose ) { 
-    write("writing some verbose output to standard error...\n\n", stderr()) 
+    write("writing some verbose output to standard error...\n", stderr()) 
 }
 
 # do some operations based on user input
-cat(paste(rnorm(opt$count, mean=opt$mean, sd=opt$sd), collapse="\n"))
+if( opt$generator == "rnorm") {
+    cat(paste(rnorm(opt$count, mean=opt$mean, sd=opt$sd), collapse="\n"))
+} else {
+    cat(paste(do.call(opt$generator, list(opt$count)), collapse="\n"))
+}
 cat("\n")
