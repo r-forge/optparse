423960b1d88217f34887deae240488e9d4fc015e
trevorld (Trevor Davis) <trevor.l.davis@gmail.com>
2013-08-21 23:30:30 +0000

    New website pointing to github
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/optparse@121 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -43,9 +43,7 @@ echo $contents; } ?>
 
 <!-- end of project description -->
 
-<p> No content added. </p>
-
-<p> The <strong>project summary page</strong> you can find <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/"><strong>here</strong></a>. </p>
+<p> Development has moved to <a href="http://www.github.com/trevorld/argparse">argparse</a>, <a href="http://www.github.com/trevorld/optparse">optparse</a>, <a href="http://www.github.com/trevorld/getopt">getopt</a>. Packages here are no longer kept up to date.</p>
 
 </body>
 </html>
